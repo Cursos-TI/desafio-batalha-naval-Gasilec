54512e9bd1b2a296f2500763c6e5506428ccff72
Gasilec <luiz.gasilec@gmail.com>
2025-06-10 18:05:27 +0000

    Desafio novato TERMINADO

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,6 +5,49 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
+    int matriz[10][10];
+
+    // Inicializar matriz com 0 (mar)
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            matriz[i][j] = 0; //faz quando a tabela for impressa aparecer só o numero 0, se n tivesse ia ser uma mini tabuada
+        }
+    }
+
+    // Posicição navio vertical (1)
+    int linha1 = 3; // A contagem sempre começa no 0, ou 2 ali, significa que vai mover 0 e 1, qe an tabela vai cair no nº3;
+    int coluna1 = 4; // pela mesma logica que em cima, vai ir ABC e aprar no D. Com os dois juntos começa na casa D3;
+    for (int i = 0; i < 3; i++) { //com isso to falando, o i que começa vale 0, acrescenta 3 vezes (0,1 e 2) e para;
+        matriz[linha1 + i][coluna1] = 3;      //aqui direciona pra onde o acrescimo vai, como ta no linha o "+ i" ele vai acrescentar 
+    }                                         //a partir do ponto inicial 3 vezes em direção ao 10, colocando o número 3 que pedi;
+
+    // Posicição navio horizontal (2)
+    int linha2 = 8; // linha 8
+    int coluna2 = 1; // coluna B
+    for (int i = 0; i < 3; i++) {
+        matriz[linha2][coluna2 + i] = 3; //como no de cima, aqui ta na coluna, então vai percorres as caras a partir da inicial, horizontalmente
+    }
+
+    // letras A a J no cabeçalho
+    printf("   "); // Espaço para alinhar com os números da esquerda
+    for (char letra = 'A'; letra <= 'J'; letra++) {
+        printf(" %c", letra);// o espaço no %c faz dar margem e um espaçamento melhor
+    }
+    printf("\n");//importante pra formatação e parecer um jogo da velha
+
+    // linhas numeradas de 1 a 10
+    for (int i = 0; i < 10; i++) {
+        printf("%2d ", i + 1); // Números da linha (1 a 10) o "i + 1"" faz a contagem começar do 1 (i = 0, 0 + 1 = 1). (o %2d deixa bonitinho e organizado)
+        for (int j = 0; j < 10; j++) {
+            printf(" %d", matriz[i][j]); //com os 2 for aqui, to fazendo imprimir a tabela 10 x 10, que já denifi no inico que apareceria paenas o nº 0;
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+
+    return 0;
+}
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
@@ -35,6 +78,3 @@ int main() {
     // 0 0 1 0 0
     // 1 1 1 1 1
     // 0 0 1 0 0
-
-    return 0;
-}
